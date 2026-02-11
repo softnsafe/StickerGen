f3868f05bf108711ca9fe4163a883a8ab895b2f1
softnsafe <37956784+softnsafe@users.noreply.github.com>
2026-02-11 18:57:22 -0500

    Update gallery.ts

--- data/gallery.ts
+++ data/gallery.ts
@@ -27,12 +27,12 @@ export const GALLERY_IMAGES: Sticker[] = [
   // -------------------------------------------------------------------------
   
   // Example Entry (Ensure you have a file at public/images/example.png to see this)
-  {
-    id: 'sticker-1770836249227',
-    url: '/images/power_ranger.png',
-    prompt: 'power ranger',
-    createdAt: 1770836249227,
-  },
+ {
+  id: 'sticker-1770854080370',
+  url: '/images/retro-sticker-of-power-ranger.png',
+  prompt: 'Retro sticker of power ranger',
+  createdAt: 1770854080370,
+},
    {
   id: 'sticker-1770846702590',
   url: 'https://drive.google.com/uc?export=view&id=1LXnk80JlyQ8acKm3BTO8PuSAHAcejvbp',
